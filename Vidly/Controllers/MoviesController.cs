ced704874f5c5ed0e91055ff1391b30e9a45960f
Arif Batuhan Yıldırımoğlu <64323875+arifbatuhanyildirimoglu@users.noreply.github.com>
2024-07-15 17:14:06 +0300

    Section 2: Exercise 1

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -8,6 +7,13 @@ namespace Vidly.Controllers
 {
 	public class MoviesController : Controller
 	{
+		public ActionResult Index()
+		{
+			var movies = GetMovies();
+
+			return View(movies);
+		}
+
 		// GET: Movies
 		public ActionResult Random()
 		{
@@ -27,26 +33,13 @@ namespace Vidly.Controllers
 			return View(viewModel);
 		}
 
-		public ActionResult Edit(int id)
+		private IEnumerable<Movie> GetMovies()
 		{
-			return Content("id=" + id);
-		}
-
-		public ActionResult Index(int? pageIndex, string sortBy)
-		{
-			if (!pageIndex.HasValue)
-				pageIndex = 1;
-
-			if (String.IsNullOrEmpty(sortBy))
-				sortBy = "Name";
-
-			return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-		}
-
-		[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-		public ActionResult ByReleaseDate(int year, int month)
-		{
-			return Content(year + "/" + month);
+			return new List<Movie>
+			{
+				new Movie {Id = 1, Name="Shrek!"},
+				new Movie {Id = 2, Name="Wall-E"},
+			};
 		}
 	}
 }
