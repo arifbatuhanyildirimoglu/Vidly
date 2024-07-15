4d5b7f39176f2661d07ff734fa9a731805c5252f
Arif Batuhan Yıldırımoğlu <64323875+arifbatuhanyildirimoglu@users.noreply.github.com>
2024-07-15 12:46:05 +0300

    Attribute routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -30,6 +30,7 @@ namespace Vidly.Controllers
 			return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
 		}
 
+		[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
 		public ActionResult ByReleaseDate(int year, int month)
 		{
 			return Content(year + "/" + month);
