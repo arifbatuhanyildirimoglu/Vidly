cf008574f17ad5a0f7abd7d0031e6cfcb6e7c634
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-16 09:15:28 +0300

    Overriding conventions

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,8 +1,12 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
 	public class Customer
 	{
 		public int Id { get; set; }
+		[Required]
+		[StringLength(255)]
 		public string Name { get; set; }
 		public bool IsSubscribedToNewsletter { get; set; }
 		public MembershipType MembershipType { get; set; }
