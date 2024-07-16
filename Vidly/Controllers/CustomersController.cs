d2610e9fde78e5e16c8f5984b9f60e21b3ac392b
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-16 20:39:08 +0300

    Updating data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -33,12 +33,23 @@ namespace Vidly.Controllers
 		}
 
 		[HttpPost]
-		public ActionResult Create(Customer customer)
+		public ActionResult Save(Customer customer)
 		{
-			_context.Customers.Add(customer);
+			if (customer.Id == 0)
+				_context.Customers.Add(customer);
+			else
+			{
+				var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+				customerInDb.Name = customer.Name;
+				customerInDb.Birthdate = customer.Birthdate;
+				customerInDb.MembershipTypeId = customer.MembershipTypeId;
+				customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+			}
+
 			_context.SaveChanges();
 
-			return RedirectToAction("Index", "Customer");
+			return RedirectToAction("Index", "Customers");
 		}
 
 		public ActionResult Index()
