2b8d5fa7b9cf6f15beaaf3e703d5f6e3f453c5c2
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-16 20:14:02 +0300

    Drop-down lists

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -21,7 +22,14 @@ namespace Vidly.Controllers
 
 		public ActionResult New()
 		{
-			return View();
+			var membershipTypes = _context.MembershipTypes.ToList();
+
+			var viewModel = new NewCustomerViewModel
+			{
+				MembershipTypes = membershipTypes,
+			};
+
+			return View(viewModel);
 		}
 
 		public ActionResult Index()
---
+++ Vidly/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+	public class NewCustomerViewModel
+	{
+		public IEnumerable<MembershipType> MembershipTypes { get; set; }
+		public Customer Customer { get; set; }
+
+	}
+}
