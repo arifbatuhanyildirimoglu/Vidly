35652ca5ee05613a76a603c7517e05ef15b497a5
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-16 19:50:49 +0300

    Labels

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,6 +9,7 @@ namespace Vidly.Models
 		[Required]
 		[StringLength(255)]
 		public string Name { get; set; }
+		[Display(Name = "Date of Birth")]
 		public DateTime? Birthdate { get; set; }
 		public bool IsSubscribedToNewsletter { get; set; }
 		public MembershipType MembershipType { get; set; }
