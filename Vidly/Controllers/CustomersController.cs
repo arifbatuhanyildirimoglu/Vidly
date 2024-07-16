5865a6ec46084774dd463fdf31d28ef8f90eefc4
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-16 09:32:58 +0300

    Querying objects

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -7,31 +6,33 @@ namespace Vidly.Controllers
 {
 	public class CustomersController : Controller
 	{
-		// GET: Customers
+		private ApplicationDbContext _context;
+
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
 		public ActionResult Index()
 		{
-			var customers = GetCustomers();
+			var customers = _context.Customers.ToList();
 
 			return View(customers);
 		}
 
 		public ActionResult Detail(int? id)
 		{
-			var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
 			if (customer == null)
 				return HttpNotFound();
 
 			return View(customer);
-		}
-
-		private IEnumerable<Customer> GetCustomers()
-		{
-			return new List<Customer>
-						{
-								new Customer { Id = 1, Name = "John Smith" },
-								new Customer { Id = 2, Name = "Mary Williams" }
-						};
 		}
 	}
 }
