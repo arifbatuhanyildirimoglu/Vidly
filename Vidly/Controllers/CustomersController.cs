6de5e50571b72511408957e7124345526c335bb1
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-16 20:27:57 +0300

    Updating data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -24,12 +24,12 @@ namespace Vidly.Controllers
 		{
 			var membershipTypes = _context.MembershipTypes.ToList();
 
-			var viewModel = new NewCustomerViewModel
+			var viewModel = new CustomerFormViewModel
 			{
 				MembershipTypes = membershipTypes,
 			};
 
-			return View(viewModel);
+			return View("CustomerForm", viewModel);
 		}
 
 		[HttpPost]
@@ -56,6 +56,22 @@ namespace Vidly.Controllers
 				return HttpNotFound();
 
 			return View(customer);
+		}
+
+		public ActionResult Edit(int? id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if (customer == null)
+				return HttpNotFound();
+
+			var viewModel = new CustomerFormViewModel
+			{
+				Customer = customer,
+				MembershipTypes = _context.MembershipTypes.ToList()
+			};
+
+			return View("CustomerForm", viewModel);
 		}
 	}
 }
