b8e478f2f33655017b4bded1b4af2ea94428989e
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-16 10:01:09 +0300

    Eager loading

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -20,7 +21,7 @@ namespace Vidly.Controllers
 
 		public ActionResult Index()
 		{
-			var customers = _context.Customers.ToList();
+			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
 			return View(customers);
 		}
