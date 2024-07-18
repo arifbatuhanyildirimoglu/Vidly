6bbe1f248fcfebb192423b638d85c842db5926b5
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-18 12:10:39 +0300

    Data Transfer Objects

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+	public class CustomerDto
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Please enter customer's name.")]
+		[StringLength(255)]
+		public string Name { get; set; }
+
+		[Min18YearsIfAMember]
+		public DateTime? Birthdate { get; set; }
+
+		public bool IsSubscribedToNewsletter { get; set; }
+
+		public byte MembershipTypeId { get; set; }
+	}
+}
