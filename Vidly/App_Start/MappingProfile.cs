d54f78b6f597355e483e288f1371dcdc63c7786d
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-24 12:06:14 +0300

    Calling an API using jQuery

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -8,7 +8,7 @@ namespace Vidly.App_Start
 	{
 		public MappingProfile()
 		{
-			Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore();
+			Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
 			Mapper.CreateMap<CustomerDto, Customer>();
 
 			Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
