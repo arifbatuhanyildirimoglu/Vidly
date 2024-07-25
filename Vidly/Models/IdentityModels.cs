76263dc20a28c7fe79894b9d1f67142f01afba5c
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-07-25 20:39:43 +0300

    Adding profile data

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -9,6 +10,10 @@ namespace Vidly.Models
 	// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
 	public class ApplicationUser : IdentityUser
 	{
+		[Required]
+		[StringLength(255)]
+		public string DrivingLicense { get; set; }
+
 		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
 		{
 			// Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -26,7 +31,7 @@ namespace Vidly.Models
 		public DbSet<MembershipType> MembershipTypes { get; set; }
 
 		public ApplicationDbContext()
-: base("DefaultConnection", throwIfV1Schema: false)
+	: base("DefaultConnection", throwIfV1Schema: false)
 		{
 		}
 
